d436af6e42190418068d612ffebcccabeff6adba
Roeland Jago Douma <unix@rullzer.com>
2011-03-10 11:33:26 +0100

    Run photoJobs in parallel
    By running all the photoJobs in parallel the friend syncing gains a huge
    speedup! There is some serialisation nessessarry but the main advantage is that
    all the pictures are fetched in parrallel.

--- resource/facebookresource.h
+++ resource/facebookresource.h
@@ -22,6 +22,7 @@
 #include <libkfacebook/userinfo.h>
 #include <Akonadi/ResourceBase>
 #include <QPointer>
+#include <QMutex>
 
 class FacebookResource : public Akonadi::ResourceBase,
                          public Akonadi::AgentBase::Observer
@@ -59,7 +60,7 @@ class FacebookResource : public Akonadi::ResourceBase,
     void detailedEventListJobFinished( KJob *job );
 
   private:
-    void fetchNextPhoto();
+    void fetchPhotos();
     void resetState();
     void abortWithError( const QString& errorMessage, bool authFailure = false );
     void abort();
@@ -81,6 +82,7 @@ class FacebookResource : public Akonadi::ResourceBase,
 
     bool mIdle;
     QPointer<KJob> mCurrentJob;
+    QMutex mPhotoMutex;
 };
 
 #endif
